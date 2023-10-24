cb095d0bea1152c7f8352ba07d08cacbc7643fd4
guilherme <guiilhermesilva466@gmail.com>
2023-10-23 23:08:58 -0300

    comit desafio 4

---
+++ desafio 4/script.js
@@ -0,0 +1,18 @@
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
+
+
+
+function onClick(element) {
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+    var captionText = document.getElementById("caption");
+    captionText.innerHTML = element.alt;
+}
