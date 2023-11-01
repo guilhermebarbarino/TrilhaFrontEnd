bae17dfcb41bf8963a8b73153059e491a793595f
guilherme <guiilhermesilva466@gmail.com>
2023-10-31 22:19:30 -0300

    ajustes

--- desafio 3/flexbox/script.js
+++ desafio 3/flexbox/script.js
@@ -1,10 +1,25 @@
-function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "/flexbox/img/menu_white_36dp.svg";
+// function menuShow() {
+//     let menuMobile = document.querySelector('.mobile-menu');
+//     if (menuMobile.classList.contains('open')) {
+//         menuMobile.classList.remove('open');
+//         document.querySelector('.icon').src = "../flexbox/img/menu_white_36dp.svg";
+//     } else {
+//         menuMobile.classList.add('open');
+//         document.querySelector('.icon').src = "../flexbox/img/close_white_36dp.svg";
+//     }
+// }
+
+function toggleMenu() {
+    // let navList = document.querySelector('.nav-list');
+    // navList.classList.toggle('open');
+
+    let navList = document.querySelector('.nav-list');
+    if (navList.classList.contains('open')) {
+        navList.classList.toggle('open');
+        navList.classList.remove('open');
+        document.querySelector('.icon').src = "../flexbox/img/menu_white_36dp.svg";
     } else {
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "/flexbox/img/close_white_36dp.svg";
+        navList.classList.add('open');
+        document.querySelector('.icon').src = "../flexbox/img/close_white_36dp.svg";
     }
 }
